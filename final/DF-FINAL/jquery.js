0d591c2eff6cac13f70dd63915ce50fb43380b77
Hissana A <hissana.ashraf@gmail.com>
2024-08-14 19:40:01 +0530

    Carousel has added- updation needed

--- final/DF-FINAL/jquery.js
+++ final/DF-FINAL/jquery.js
@@ -19,4 +19,68 @@ $(document).ready(function () {
   });
 });
 
-//
+//CAROUSEL
+const track = document.querySelector(".carousel-track");
+const slides = Array.from(track.children);
+const prevButton = document.querySelector(".carousel-button--left");
+const nextButton = document.querySelector(".carousel-button--right");
+const dotsNav = document.querySelector(".carousel-nav");
+const dots = Array.from(dotsNav.children);
+
+//arranging the slides next to one another
+const slideWidth = slides[0].getBoundingClientRect().width;
+const setSlidePosition = (slide, index) => {
+  slide.style.left = slideWidth * index + "px";
+};
+slides.forEach(setSlidePosition);
+
+//moving slides
+const moveToSlides = (track, currentSlide, targetSlide) => {
+  track.style.transform = "translateX(-" + targetSlide.style.left + ")";
+  currentSlide.classList.remove("current-slide");
+  targetSlide.classList.add("current-slide");
+};
+// moving indicators
+const updateDots = (currentDot, targetDot) => {
+  currentDot.classList.remove("current-slide");
+  targetDot.classList.add("current-slide");
+};
+//moving slide to right
+nextButton.addEventListener("click", (e) => {
+  const currentSlide = track.querySelector(".current-slide");
+  const nextSlide = currentSlide.nextElementSibling;
+  const currentDot = dotsNav.querySelector(".current-slide");
+  const nextDot = currentDot.nextElementSibling;
+
+  //move to next slide
+  moveToSlides(track, currentSlide, nextSlide);
+  updateDots(currentDot, nextDot);
+});
+
+//moving the slide to left
+prevButton.addEventListener("click", (e) => {
+  const currentSlide = track.querySelector(".current-slide");
+  const prevSlide = currentSlide.previousElementSibling;
+  const currentDot = dotsNav.querySelector(".current-slide");
+  const prevDot = currentDot.previousElementSibling;
+
+  //move to next slide
+  moveToSlides(track, currentSlide, prevSlide);
+  updateDots(currentDot, prevDot);
+});
+
+//nav indicators move uopn slides-------------------------------------------------
+
+dotsNav.addEventListener("click", (e) => {
+  const targetDot = e.target.closest("button");
+
+  if (!targetDot) return;
+
+  const currentSlide = track.querySelector(".current-slide");
+  const currentDot = dotsNav.querySelector(".current-slide");
+  const targetIndex = dots.findIndex((dot) => dot === targetDot);
+  const targetSlide = slides[targetIndex];
+
+  moveToSlides(track, currentSlide, targetSlide);
+  updateDots(currentDot, targetDot);
+});
